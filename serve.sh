a286a9a21c019562035307a8a12cac73ad92149b
alanger <alanger@ebay.com>
2020-09-20 14:29:29 +0300

    make serve.sh work on macOS (Just because they decided for no apparent reason to not have the readlink -f operand)

--- serve.sh
+++ serve.sh
@@ -4,7 +4,20 @@ if [[ ! -x "$(command -v docker)" ]]; then
     exit 1
 fi
 
-if [[ ! -d $(readlink -f ./web) ]]; then
+# emulate readlink -f in order for the script to work on macOS (which fails to have the -f operand) and not just Linux. sigh.
+DIR=./web
+cd `dirname $DIR`
+DIR=`basename $DIR`
+while [ -L "$DIR" ]
+do
+    DIR=`readlink $DIR`
+    cd `dirname $DIR`
+    DIR=`basename $DIR`
+done
+PHYS_DIR=`pwd -P`
+ROOT_DIR=$PHYS_DIR/$DIR
+
+if [[ ! -d $ROOT_DIR ]]; then
     echo "Run the deploy.sh script first!"
     exit 1
 fi
@@ -15,5 +28,5 @@ if [[ "$1" != "" ]]; then
     PORT="$1"
 fi
 
-echo "Launching FGrav server at http://localhost:$PORT"
-docker container run -v $(readlink -f ./web):/usr/share/nginx/html:ro -p $PORT:80 nginx
+echo "Launching FGrav server at http://localhost:$PORT from $ROOT_DIR"
+docker container run -v $ROOT_DIR:/usr/share/nginx/html:ro -p $PORT:80 nginx
